b62dc6ede943a1d82a9c273dd78bc32baf8af4e6
SARVESH181 <98431910+SARVESH181@users.noreply.github.com>
2022-10-02 12:43:13 -0400

    Add files via upload

---
+++ update.sql
@@ -0,0 +1,27 @@
+update patient_treatment set treatment = 'Radiology' where doctor_id='DOC-101';
+update patient_treatment set treatment = 'Myopathy' where doctor_id='DOC-102';
+update patient_treatment set treatment = 'Diagnosis' where doctor_id='DOC-103';
+update patient_treatment set treatment = 'Removal' where doctor_id='DOC-104';
+update patient_treatment set treatment = 'Prognosis' where doctor_id='DOC-105';
+update patient_treatment set treatment = 'Biopsy' where doctor_id='DOC-106';
+update patient_treatment set treatment = 'Lapro' where doctor_id='DOC-107';
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-100';
+
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-101';
+
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-102';
+
+update doctor_records set doctor_status = 'Not Available' where doctor_id='DOC-103';
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-104';
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-105';
+
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-106';
+
+update doctor_records set doctor_status = 'Available' where doctor_id='DOC-107';
+
